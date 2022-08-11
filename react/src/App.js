55fedf15743637dcc40026955d081ccff5699a9f
Jackey Yang <jackey2870@gmail.com>
2022-08-11 16:29:19 -0500

    used axios to get data

--- react/src/App.js
+++ react/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [result, setResult] = useState("");
+  //
+  // set prefix to proxy/url (see default as example)
+  // WARNING: YOU MUST INCLUDE THE '/' AFTER PREFIX
+  //
+  const prefix = "http://localhost:5000/https://lindogourmet.com/";
+
+  useEffect(() => {
+    axios.get(prefix + "app/category/list").then((res) => {
+      setResult(res.data);
+      //
+      // check console for data; if no data is displayed, check your proxy address and url
+      //
+      console.log(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //
+    // example and arbitrary data display
+    // make sure to check your data values and adapt this code to what you need
+    //
+    <div>
+      {result ? (
+        result.map((x) => <li key={x.categoryEntityId}>{x.label}</li>)
+      ) : (
+        <h1>No data</h1>
+      )}
     </div>
   );
 }
